82a8725b382f9c4616c502f0ea749bf3a8cf0304
Kate Bricken <katebricken@icloud.com>
2025-08-29 05:08:38 -0500

    Update HW2Code.py

--- Lab02/HW2Code.py
+++ Lab02/HW2Code.py
@@ -2,8 +2,18 @@
 ##Part 1 - Take the following list and multiply all list items together.
 part1 = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
 
+Product = 1
+for num in part1:
+    Product *=num
+print("Product of List:",Product)
+
 ##Part 2 - Take the following list and add all list items together.
 part2 = [-1, 23, 483, 8573, -13847, -381569, 1652337, 718522177]
+
+Total = 0
+for num in part2:
+    Total += num
+print("Sum of List:",Total)
 
 ##Part 3 - Take the following list and only add together those list items which are even. 
 ##You can use the following snippet of code to see if a number is even or odd. 
@@ -11,4 +21,8 @@ part2 = [-1, 23, 483, 8573, -13847, -381569, 1652337, 718522177]
 ##We divide by 2 and look at the remainder; if there is no remainder the number is even, if there is a remainder the number is odd.
 part3 = [146, 875, 911, 83, 81, 439, 44, 5, 46, 76, 61, 68, 1, 14, 38, 26, 21] 
 
-isEven = num1 % 2 == 0
+Even_Sum = 0
+for num in part3:
+    if num % 2 == 0: 
+        Even_Sum += num
+print("Sum of even numbers:", Even_Sum)
