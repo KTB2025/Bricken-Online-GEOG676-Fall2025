fd6a8283fad2b4ba7c11a728083f90e89edd14a0
Kate Bricken <katebricken@icloud.com>
2025-08-30 10:55:36 -0500

    Update HW3Code.py

--- Lab03/HW3Code.py
+++ Lab03/HW3Code.py
@@ -3,7 +3,7 @@
 ##Create a class for each shape found in the text file (20pt)
 ##For each line, create a new object determined by the shape (e.g. Triangle object for line Triangle,8,1 base and height) (30pt)
 ##Iterate through your list and print out the area for each shape (30pt)
-
+import math
 #Define Shape Classes
 class Shape():
     def getarea(self):
@@ -20,7 +20,7 @@ class Circle(Shape):
     def __init__(self,r):
         self.radius = r
     def getarea(self):
-        return 3.14 * self.radius * self.radius
+        return math.pi * self.radius * self.radius
     
 class Triangle(Shape):
     def __init__(self,b,h):
@@ -37,15 +37,15 @@ file.close()
 #For each line, create a new object determined by the shape & print out the area for each shape
 for line in lines:
     components = line.split(',')
-    Shape = components[0]
+    Shape_Type = components[0].strip()
 
-    if Shape == 'Rectangle':
+    if Shape_Type == 'Rectangle':
         rect = Rectangle(float(components[1]),float(components[2]))
         print('Area of Rectangle is:',rect.getarea())
-    elif Shape == 'Circle':
+    elif Shape_Type == 'Circle':
         circl = Circle(float(components[1]))
         print('Area of Circle is:',circl.getarea())
-    elif Shape == 'Triangle':
+    elif Shape_Type == 'Triangle':
         tri = Triangle(float(components[1]),float(components[2]))
         print('Area of triangle is:',tri.getarea())
     else:
