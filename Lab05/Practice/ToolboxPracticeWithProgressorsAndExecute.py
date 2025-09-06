f6a959ad9d3b883e5fd215d3290c176594b82f95
Kate Bricken <katebricken@icloud.com>
2025-09-06 08:46:45 -0500

    updating course code and practice
    Refactor updateMessages method and improve building validation.

--- Lab05/Practice/ToolboxPracticeWithProgressorsAndExecute.py
+++ Lab05/Practice/ToolboxPracticeWithProgressorsAndExecute.py
@@ -83,94 +83,111 @@ class BuildingProximityTool(object):
     def isLicensed(self):
         return True
 
-    def updateParameters(self, parameters):
-        # Normalize names (no spaces)
-        for idx in (4, 5):
-            if parameters[idx].value:
-                parameters[idx].value = str(parameters[idx].value).strip().replace(" ", "_")
-        return
+def updateMessages(self, parameters):
+    """Modify the messages created by internal validation for each tool
+    parameter.  This method is called after internal validation."""
+    return
 
-    def updateMessages(self, parameters):
-        # Warn if spatial reference mismatch
-        if parameters[0].value and parameters[1].value:
-            try:
-                sr_gar = arcpy.Describe(parameters[0].value).spatialReference
-                sr_bld = arcpy.Describe(parameters[1].value).spatialReference
-                if sr_gar.name != sr_bld.name:
-                    parameters[0].setWarningMessage(
-                        f"Garage SR ({sr_gar.name}) differs from Buildings SR ({sr_bld.name}). "
-                        "Outputs will use Buildings SR."
-                    )
-            except Exception:
-                pass
-        return
+def updateMessages(self, parameters):
+    for param in parameters:
+        if param.name == "buildingNumber":
+            # We've found the correct parameter
+            buildingNum = param.value
+    return
+
+def updateMessages(self, parameters):
+    for param in parameters:
+        if param.name == "buildingNumber":
+            buildingNum = param.value
+            campus = r"https://github.com/KTB2025/Bricken-Online-GEOG676-Fall2025/tree/5bd1938fc56f24fa0dd7c422723a3ce33f3228ed/Lab04/data/Campus.gdb"
+            where_clause = "Bldg = '%s'" % buildingNum
+            cursor = arcpy.SearchCursor(campus + "/Structures", where_clause=where_clause)
+    return   
+
+
+def updateMessages(self, parameters):
+    for param in parameters:
+        if param.name == "buildingNumber":
+            buildingNum = param.value
+            campus = r"https://github.com/KTB2025/Bricken-Online-GEOG676-Fall2025/tree/5bd1938fc56f24fa0dd7c422723a3ce33f3228ed/Lab04/data/Campus.gdb"
+            where_clause = "Bldg = '%s'" % buildingNum
+            cursor = arcpy.SearchCursor(campus + "/Structures", where_clause=where_clause)
+            count = 0
+            for row in cursor:
+                count += 1
+            if count == 0:
+                param.setErrorMessage("Cannot find building %s in Structures" % buildingNum)
+    return
 
     # ----------------------------
     # Execute
     # ----------------------------
-    def execute(self, parameters, messages):
-        """Perform buffer + intersect workflow."""
-
-        # Inputs
-        garage_fc = parameters[0].valueAsText
-        bldg_fc   = parameters[1].valueAsText
-        buffer_m  = float(parameters[2].value)
-        out_ws    = parameters[3].valueAsText
-        out_buff_name = parameters[4].valueAsText
-        out_intersect = parameters[5].valueAsText
-
-        # Env
-        arcpy.env.overwriteOutput = True
-
-        # Paths
-        out_buff_path = os.path.join(out_ws, out_buff_name)
-        out_intersect_path = os.path.join(out_ws, out_intersect)
-
-        # Progressor setup
+        def execute(self, parameters, messages):
+        """The source code of the tool."""
+        # Define our progressor variables
+        readTime = 2.5
         start = 0
         maximum = 100
-        step = 50
-        pause = 0.5
+        step = 25
 
-        arcpy.SetProgressor("step", "Preparing inputs…", start, maximum, step)
-        time.sleep(pause)
-        arcpy.AddMessage("Preparing inputs…")
+        # Setup the progressor
+        arcpy.SetProgressor("step", "Checking building proximity...", start, maximum, step)
+        time.sleep(readTime)
+        # Add message to the results pane
+        arcpy.AddMessage("Checking building proximity...")
 
-        # Match SR: project garage points if needed
-        gar_sr = arcpy.Describe(garage_fc).spatialReference
-        bld_sr = arcpy.Describe(bldg_fc).spatialReference
-        garage_for_buffer = garage_fc
+        campus = r"D:/DevSource/Tamu/GeoInnovation/_GISProgramming/data/modules/17/Campus.gdb"
+        
+        # Setup our user input variables
+        buildingNumber_input = parameters[0].valueAsText
+        bufferSize_input = int(parameters[1].value)
 
-        if gar_sr.name != bld_sr.name and bld_sr.name not in (None, "", "Unknown"):
-            projected_garage = os.path.join(out_ws, "Garage_Points_Reprojected")
-            if arcpy.Exists(projected_garage):
-                arcpy.Delete_management(projected_garage)
-            arcpy.Project_management(garage_fc, projected_garage, bld_sr)
-            garage_for_buffer = projected_garage
-            arcpy.AddMessage(f"Reprojected garage points to {bld_sr.name}.")
+        # Generate our where_clause
+        where_clause = "Bldg = '%s'" % buildingNumber_input
 
-        # Buffer garages
+        # Check if building exists
+        structures = campus + "/Structures"
+        cursor = arcpy.SearchCursor(structures, where_clause=where_clause)
+        shouldProceed = False
+
+        # Increment the progressor and change the label; add message to the results pane
         arcpy.SetProgressorPosition(start + step)
-        arcpy.SetProgressorLabel("Buffering garage points…")
-        time.sleep(pause)
-        dist_str = f"{buffer_m} Meters"
-        if arcpy.Exists(out_buff_path):
-            arcpy.Delete_management(out_buff_path)
-        arcpy.Buffer_analysis(garage_for_buffer, out_buff_path, dist_str, dissolve_option="NONE")
-        arcpy.AddMessage(f"Garage buffers created → {out_buff_path}")
+        arcpy.SetProgressorLabel("Validating building number once more...")
+        time.sleep(readTime)
+        arcpy.AddMessage("Validating building number once more...")
 
-        # Intersect
-        arcpy.SetProgressorPosition(start + 2*step)
-        arcpy.SetProgressorLabel("Intersecting buffers with Buildings…")
-        time.sleep(pause)
-        if arcpy.Exists(out_intersect_path):
-            arcpy.Delete_management(out_intersect_path)
-        arcpy.Intersect_analysis([out_buff_path, bldg_fc], out_intersect_path, join_attributes="ALL")
-        arcpy.AddMessage(f"Intersect done → {out_intersect_path}")
+        for row in cursor:
+            if row.getValue("Bldg") == buildingNumber_input:
+                shouldProceed = True
 
-        arcpy.SetProgressorPosition(maximum)
-        arcpy.SetProgressorLabel("Finished")
-        time.sleep(pause)
-        arcpy.AddMessage("Tool completed successfully.")
 
+        # If we shouldProceed do so
+        if shouldProceed:
+            # Generate the name for our generated buffer layer
+            buildingBuff = "/building_%s_buffed_%s" % (buildingNumber_input, bufferSize_input)
+            # Get reference to building
+            buildingFeature = arcpy.Select_analysis(structures, campus + "/building_%s" % (buildingNumber_input), where_clause)
+            # Buffer the selected building
+            arcpy.Buffer_analysis(buildingFeature, campus + buildingBuff, bufferSize_input)
+            # Increment the progressor, change label, output message to results pane too
+            arcpy.SetProgressorPosition(start + step)
+            arcpy.SetProgressorLabel("Buffering....")
+            time.sleep(readTime)
+            arcpy.AddMessage("Buffering...")
+            # Clip the structures to our buffered feature
+            arcpy.Clip_analysis(structures, campus + buildingBuff, campus + "/clip_%s" % (buildingNumber_input))
+            # Increment the progressor, change label, output message to results pane too
+            arcpy.SetProgressorPosition(start + step)
+            arcpy.SetProgressorLabel("Clipping....")
+            time.sleep(readTime)
+            arcpy.AddMessage("Clipping...")
+            # Remove the feature class we just created
+            arcpy.Delete_management(campus + "/building_%s" % (buildingNumber_input))
+            # Increment the progressor, change label, output message to results pane too
+            arcpy.SetProgressorPosition(maximum)
+            arcpy.SetProgressorLabel("Cleaning up files....")
+            time.sleep(readTime)
+            arcpy.AddMessage("Cleaning up files...")
+        else:
+            print("Seems we couldn't find the building you entered")
         return None
