32cb87b6c2092eb6aee3a94dd74175ce7dd93a3c
Kate Bricken <katebricken@icloud.com>
2025-09-08 03:26:57 -0500

    Update HW06MapTool.py

--- Lab06/HW06MapTool.py
+++ Lab06/HW06MapTool.py
@@ -11,18 +11,54 @@ class Toolbox:
         self.alias = "toolbox"
 
         # List of tool classes associated with this toolbox
-        self.tools = [Tool]
+        self.tools = [GraduatedColorsRenderer]
 
 
-class Tool:
+class GraduatedColorsRenderer:
     def __init__(self):
         """Define the tool (tool name is the name of the class)."""
-        self.label = "Tool"
-        self.description = ""
+        self.label = "Graduated Colors Renderer"
+        self.description = "Applies a graduated colors renderer to a layer."
 
     def getParameterInfo(self):
         """Define the tool parameters."""
-        params = None
+        param(0) = arcpy.Parameter(
+            displayName="Input ArcGIS Pro Prooject Name",
+            name="aepxinputname",
+            datatype="DEFile",
+            parameterType="Required",
+            direction="Input")
+        
+        param(1) = arcpy.Parameter(
+            displayName="Layer to Classsify",
+            name="ClassificationLayer",
+            datatype="DEFeatureLayer",
+            parameterType="Required",
+            direction="Input")
+        
+        param(2) = arcpy.Parameter(
+            displayName="Output Location",
+            name="OutputLocation",   
+            datatype="DEWorkspace",        
+            parameterType="Required",
+            direction="Input")
+        
+         param(3) = arcpy.Parameter(
+            displayName="Output Project Name",
+            name="OutProjectName",   
+            datatype="GPString",        
+            parameterType="Required",
+            direction="Input")
+            
+
+        param(4) = arcpy.Parameter(
+            displayName="Field Name",
+            name="fieldname",   
+            datatype="GPString",        
+            parameterType="Required",
+            direction="Input")
+
+        params = [param0, param1, param2, param3, param4]
         return params
 
     def isLicensed(self):
@@ -42,6 +78,29 @@ class Tool:
 
     def execute(self, parameters, messages):
         """The source code of the tool."""
+        
+        #Define Progressor Variables
+        readTime = 2.5
+        start = 0
+        max = 100
+        step = 33
+
+        #Setup Progressor 
+        arcpy.SetProgressor("step", "Starting Process...", start, max, step)
+        tme.sleep(readTime)
+
+        arcpy.AddMessage("Validating project file...")
+
+        project = arcpy.mp.ArcGISProProject(parameters[0].valueAsText)
+
+        #Grabs the first instance of a map from the .aprx
+        campus = project.listMaps('map')[0] #user navigates to this specified folder
+
+        #Increment Progressor
+        arcpy.SetProgressorPosition(start + step)
+        arcpy. SetProgressorLabel('Finding your map layer...'))
+        time.sleep(readTime)
+        arcpy.AddMessage("Finding your map layer...")
         return
 
     def postExecute(self, parameters):
