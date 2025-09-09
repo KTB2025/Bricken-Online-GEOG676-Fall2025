1ef475d1949c15118190e110e4919e4eee8118b4
Kate Bricken <katebricken@icloud.com>
2025-09-09 06:05:33 -0500

    Lab07 walkthrough instructions

--- Lab07/HW7Code.py
+++ Lab07/HW7Code.py
@@ -1,12 +1,46 @@
-#TAMU GIS Programming: Homework 07 - Raster Analysis
-##7A
-###Find and download satellite imagery that is split up by band. Landsat is a great choice. Add the imagery to your Github.
-###Using the satellite imagery you downloaded, create a composite raster image
-###Find a digital elevation model of your area of interest and create the following: Shace-Shuttle Radar Topography Mission is a great choice. Add the DEM to your Github.
-####A hillshade analysis raster
-####A slope analysis raster
-##7B
-###Find and download satellite imagery that is split up by band
-###Create a composite image using the rasters you downloaded
-###Make a clipping mask polygon of the darkest area you can find
-###reate an atmospherically corrected multiband raster
+# =============================================
+# TAMU GIS Programming: Homework 07 - Raster Analysis
+# Author: Kate Bricken
+# Date: 09/08/2025
+# =============================================
+
+import arcpy
+import os
+
+# R5 Composite Bands 
+
+source = r"C:\Mac\Home\Documents\GEOG 676\Lab07\LT05_L1TP_026039_20110819_20200820_02_T1"
+band1 = arcpy.sa.Raster(source + r"\LT05_L1TP_026039_20110819_20200820_02_T1_B1.TIF") # blue 
+band2 = arcpy.sa.Raster(source + r"\LT05_L1TP_026039_20110819_20200820_02_T1_B2.TIF") # gree
+band3 = arcpy.sa.Raster(source + r"\LT05_L1TP_026039_20110819_20200820_02_T1_B3.TIF") # red
+band4 = arcpy.sa.Raster(source + r"\LT05_L1TP_026039_20110819_20200820_02_T1_B4.TIF") # NIR
+
+#Delete existing composite output
+arcpy.env.overwriteOutput = True
+
+out_composite = os.path.join(source, "combined.tif")
+if arcpy.Exists(out_composite):
+    arcpy.management.Delete(out_composite)
+
+arcpy.management.CompositeBands([band1, band2, band3, band4], out_composite)
+
+composite = arcpy.CompositeBands_management([band1, band2, band3, band4], source + "\combined.tif")
+
+
+# Hillshade
+source = r"C:\Mac\Home\Documents\GEOG 676\Lab07\DEM"
+azimuth = 315
+altitude = 45
+shadows = "NO_SHADOWS"
+z_factor = 1
+arcpy.ddd.HillShade(source + r"\n30_w097_1arc_v3.tif", source + r"\n30_w097_1arc_v3_hillshade.tif", azimuth, altitude, shadows, z_factor)
+
+
+
+# Slope
+source = r"C:\Mac\Home\Documents\GEOG 676\Lab07\DEM"
+output_measurement = "DEGREE"
+z_factor = 1
+# method = "PLANAR"
+# z_unit = "METERS"
+arcpy.ddd.Slope(source + r"\n30_w097_1arc_v3.tif", source + r"\n30_w097_1arc_v3_slope.tif", output_measurement, z_factor)
